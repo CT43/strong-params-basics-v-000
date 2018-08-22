e7f90f8b4aa3a715a8ae2cee11c7c2d5da12e7b9
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-22 21:58:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,10 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
 	end
 
+	def edit
+	@post = Post.find(params[:id])
+end
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
